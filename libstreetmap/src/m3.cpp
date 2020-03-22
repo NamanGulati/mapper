bcc22c659307d1c61b61a56d0d5260d026a014ee
Robert Chinezon <r.chinezon@mail.utoronto.ca>
2020-03-21 21:21:33 -0400

    First attempt at dijkstra

--- libstreetmap/src/m3.cpp
+++ libstreetmap/src/m3.cpp
@@ -1,6 +1,20 @@
 #include "m3.h"
 #include "StreetsDatabaseAPI.h"
+#include <queue>
 #include "globals.h"
+#include "m1.h"
+#include "helpers.h"
+
+typedef std::pair<int, int> intPair;
+
+class pairCompareIntInt
+{ 
+public: 
+    int operator() (const intPair& p1, const intPair& p2) 
+    { 
+        return p1.second > p2.second; 
+    } 
+}; 
 
 double compute_path_travel_time(const std::vector<StreetSegmentIndex>& path, const double turn_penalty){
 
@@ -26,8 +40,47 @@ double compute_path_walking_time(const std::vector<StreetSegmentIndex>& path, co
 }
 
 std::vector<StreetSegmentIndex> find_path_between_intersections(const IntersectionIndex intersect_id_start, const IntersectionIndex intersect_id_end, const double turn_penalty){
+    int nodeLength = adjacencyList.size();
+    int dist[nodeLength], parent[nodeLength];
+    int top;
+    int topWeight;
     std::vector<StreetSegmentIndex> path;
-    path.push_back(1);
-    path.push_back(2);
+    
+    
+    for (int x = 0; x < nodeLength; x ++){
+        dist[x] = INT_MAX;
+        parent[x] = -1;
+    }
+    dist[intersect_id_start] = 0;
+    
+    std::priority_queue < intPair, std::vector<intPair>, pairCompareIntInt > pq;
+    pq.push(std::make_pair(intersect_id_start, dist[intersect_id_start]));
+    
+    while (!pq.empty()){
+        top = pq.top().first; //break when end is on top
+        if (top = intersect_id_end)
+            break;
+        topWeight = pq.top().second;
+        pq.pop();
+         
+        for (int x = 0; x < adjacencyList[top].size(); x ++){
+            int interId = adjacencyList[top][x].first;
+            std::pair<int, int> currInter(interId, find_street_segment_travel_time(adjacencyList[top][x].second));
+            if (dist[currInter.first] > dist[top] + currInter.second) 
+            { 
+                // Updating distance of current Intersection 
+                dist[currInter.first] = dist[top] + currInter.second; 
+                parent[currInter.first] = top; //keep track of path
+                pq.push(std::make_pair(currInter.first, dist[currInter.first]));
+            } 
+        }
+        
+    }
+    int x = top;
+    while(parent[x] != -1){
+        path.push_back(parent[x]);
+        x = parent[x];
+    }
+    std::reverse(path.begin(), path.end());
     return path;
 }
