f42ac93574e5125b85e0461d575898b7db65f3b0
Gulati  Naman <gulatin4@ug220.eecg.toronto.edu>
2020-04-01 11:25:52 -0400

    m4 init

---
+++ libstreetmap/src/m4.cpp
@@ -0,0 +1,6 @@
+#include "m4.h"
+
+
+std::vector<CourierSubpath> traveling_courier(const std::vector<DeliveryInfo>& deliveries, const std::vector<int>& depots, const float turn_penalty, const float truck_capacity){
+	return std::vector<DeliveryInfo> ();
+}
