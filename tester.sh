a49a80a268526e0f5404839e15d986201c29f71a
Gulati  Naman <gulatin4@ug220.eecg.toronto.edu>
2020-04-07 19:31:57 -0400

    tester script added

---
+++ tester.sh
@@ -0,0 +1,6 @@
+ece297exercise 4 --run_tester M4_Public_Hard_Toronto
+ece297exercise 4 --run_tester M4_Public_Hard_NewYork
+ece297exercise 4 --run_tester M4_Public_Hard_London
+ece297exercise 4 --run_tester M4_Public_Extreme_Toronto
+ece297exercise 4 --run_tester M4_Public_Extreme_NewYork
+ece297exercise 4 --run_tester M4_Public_Extreme_London
